c7d5699e7cccc1ccb3e0de92eb2c81939ed661f7
JOEYLEE1121 <joeylee1121@gmail.com>
2022-08-03 23:40:11 +0900

    .

--- discord.py
+++ discord.py
@@ -11,7 +11,7 @@ DISCORD_URL_CME = os.environ["DISCORD_URL_CME"]
 
 # for short msg
 def toast(msg, name) -> None:
-
+    return msg
     if (name == "OB"):
         requests.post(DISCORD_URL_OB, json={"username": "OB alert", "content": msg})
 
