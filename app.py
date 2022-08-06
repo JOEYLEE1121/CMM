6b6189997e098ac70cfc7258a11381396b2380d0
JOEYLEE1121 <joeylee1121@gmail.com>
2022-08-06 17:56:43 +0900

    remove webhook debug

--- app.py
+++ app.py
@@ -31,7 +31,9 @@ def webhook():
     data = json.loads(request.data)
     data_str = json.dumps(data, indent=4)
     logging.info(data_str)
-    dc.toast(":incoming_envelope:\n```json\n{}```".format(data_str), data)
+
+    # for debugging webhooks:
+    # dc.toast(":incoming_envelope:\n```json\n{}```".format(data_str), data)
 
     # check passphrase
     if data["passphrase"] != WEBHOOK_PASSPHRASE:
@@ -61,12 +63,15 @@ def webhook():
         order = api.submit_order(symbol, quantity, side, "limit", "gtc", limit_price)
         logging.info("Got order back from Alpaca")
         logging.info(order)
-        dc.toast(":ok: Got response `order` from Alpaca\n```json\n{}```".format(order), data)
+
+        # for debugging webhooksL
+        # dc.toast(":ok: Got response `order` from Alpaca\n```json\n{}```".format(order), data)
     except:
         logging.error("Alpaca responded with error")
         logging.error(order)
         return {"code": "error", "message": "sth wrong with alpaca api"}
 
+    
     dc.toast(
         ":white_check_mark: strategy {} triggered: {} {} {} at {}".format(name, side, quantity, symbol, limit_price), data
     )
