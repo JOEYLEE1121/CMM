2a87f8a5e74b96636bd0c4909f614edbf6dbc284
LemuelKL <lemuellee.kl@gmail.com>
2022-08-26 22:42:44 +0800

    more scalable error handling

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from env import WEBHOOK_PASSPHRASE
-from flask import Flask, render_template, request
-import werkzeug
+from flask import Flask, request
 import logging
 import json
 import dc
 import gsheet as gs
 import alpaca
 from order import Order
+from error_handlers import UnauthorizedRequest, BadIncomingJSON, blueprint
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
+app.register_blueprint(blueprint)
 
 
 @app.route("/webhook", methods=["POST"])
@@ -20,7 +21,7 @@ def webhook():
         pwd = data["passphrase"]
         order = Order(data)
     except:
-        raise BadIncomingJSON(data)
+        raise BadIncomingJSON(request.data)
 
     if pwd != WEBHOOK_PASSPHRASE:
         raise UnauthorizedRequest()
@@ -34,30 +35,3 @@ def webhook():
     return "OK", 200
 
 
-class UnauthorizedRequest(werkzeug.exceptions.HTTPException):
-    code = 401
-    description = "Incorrect/missing credentials."
-
-
-@app.errorhandler(UnauthorizedRequest)
-def handle_unauthorized_request(e):
-    return e.description, e.code
-
-
-class BadIncomingJSON(werkzeug.exceptions.HTTPException):
-    code = 400
-    description = "Unable to parse incoming JSON."
-
-    def __init__(self, json_data):
-        self.json = json_data
-
-
-@app.errorhandler(BadIncomingJSON)
-def handle_bad_incoming_json(e):
-    json_str = json.dumps(e.json, indent=4)
-    gs.log_error("Unable to parse incoming JSON: {}".format(json_str))
-    dc.TOAST.send(
-        username="Error",
-        content=":warning: **BadIncomingJSON** ```json\n{}```".format(json_str),
-    )
-    return e.description, e.code
---
+++ error_handlers.py
@@ -0,0 +1,36 @@
+import werkzeug
+import dc
+import gsheet as gs
+
+from flask import Blueprint
+
+blueprint = Blueprint("error_handlers", __name__)
+
+
+class UnauthorizedRequest(werkzeug.exceptions.HTTPException):
+    code = 401
+    description = "Incorrect/missing credentials."
+
+
+@blueprint.app_errorhandler(UnauthorizedRequest)
+def handle_unauthorized_request(e):
+    return e.description, e.code
+
+
+class BadIncomingJSON(werkzeug.exceptions.HTTPException):
+    code = 400
+    description = "Unable to parse incoming JSON."
+
+    def __init__(self, json_str):
+        self.json_str = json_str
+
+
+@blueprint.app_errorhandler(BadIncomingJSON)
+def handle_bad_incoming_json(e):
+    json_str = e.json_str
+    gs.log_error("Unable to parse incoming JSON: {}".format(json_str))
+    dc.TOAST.send(
+        username="Error",
+        content=":warning: **BadIncomingJSON** ```json\n{}```".format(json_str),
+    )
+    return e.description, e.code
